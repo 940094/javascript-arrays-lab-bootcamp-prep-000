86eb548801b8898244aa873e7b27e4704d3eae59
Justin Shaw <easyworkemail@gmail.com>
2019-04-24 14:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
+function destructivelyAppendKitten(kittens) {
+  kittens.push("Garfee") // add a kitten using push
+  return name
+}
